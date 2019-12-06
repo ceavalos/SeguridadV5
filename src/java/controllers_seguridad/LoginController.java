ee6a4bd089d87438e65088904e298b3811d2f283
Carlos Avalos <Carlos Avalos@192.168.0.14>
2019-12-05 19:29:54 -0600

    se agrego seleccion de rol en el login

--- src/java/controllers_seguridad/LoginController.java
+++ src/java/controllers_seguridad/LoginController.java
@@ -10,10 +10,13 @@ import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import modelos_seguridad.Empresa;
+import modelos_seguridad.Roles;
 import modelos_seguridad.Usuarios;
 import modelos_seguridad.UsuariosRoles;
 import persistenciaSeguridad_ejb.EmpresaFacadeLocal;
+import persistenciaSeguridad_ejb.RolesFacadeLocal;
 import persistenciaSeguridad_ejb.UsuariosFacadeLocal;
+import persistenciaSeguridad_ejb.UsuariosRolesFacadeLocal;
 
 @Named
 @ViewScoped
@@ -23,19 +26,48 @@ public class LoginController implements Serializable{
     UsuariosFacadeLocal usuario_EJB; 
     @EJB
     EmpresaFacadeLocal Empresa_EJB;
-       
-    private Usuarios usuario;
-    private Empresa emp;
+    @EJB
+    RolesFacadeLocal roles_EJB;
+    @EJB
+    UsuariosRolesFacadeLocal user_rolEJB;
     
+    private Usuarios usuario;
+    
+    private Empresa emp;    
     private List<Empresa> empresaList;
 
+    private Roles role;
+    private List<Roles> rolesList;
+    
     
     @PostConstruct
     public void init(){
         usuario    = new Usuarios();           
-        emp        = new Empresa();             
-        buscaEmpresa();                
+        emp        = new Empresa();       
+        role      = new Roles();
+        empresaList= new ArrayList<Empresa>();
+        rolesList  = new ArrayList<Roles>();
+        //buscaEmpresa();    
+        //populateRoles();
     }
+
+    public Roles getRoles() {
+        return role;
+    }
+
+    public void setRoles(Roles roles) {
+        this.role = roles;
+    }
+
+    public List<Roles> getRolesList() {
+        return rolesList;
+    }
+
+    public void setRolesList(List<Roles> rolesList) {
+        this.rolesList = rolesList;
+    }
+
+   
 
     public Empresa getEmp() {
         return emp;
@@ -85,20 +117,67 @@ public class LoginController implements Serializable{
         return redireccion;
     }
     
-    public List<Empresa> buscaEmpresa(){
-      /*userList=usuario_EJB.findAll();
-      Usuarios us = (Usuarios) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuarios");
-       
-       for(Usuarios m : userList ){
-           
-           if (m.getCodUsuario().equals(us.getCodUsuario()) && m.getEstado().equals("A") ){
-               empresaList.add(m.getIdEmpresa());
-               //System.out.println("Estamos dentro  del if del loop " + m.getIdEmpresa().getIdEmpresa() );
-           }
-       }*/  
-      //System.out.println("Estamos en LoginController");
+    public List<Empresa> buscaEmpresa(){      
         empresaList = Empresa_EJB.findAll();
         return empresaList;
     }
+    
+    public List<Empresa> populateEmpresa(){
+        List<Empresa> empresa  = Empresa_EJB.findAll();
+        List<Usuarios> usuario = usuario_EJB.findAll();        
+        //System.out.println("Dentro de populateEmpresa Antes de empresaList.add(null) ");
+        empresaList.clear();        
+        for (Usuarios u : usuario){
+            //System.out.println(" dentro del for " + u.getCodUsuario()+ " usuario en variable " + this.usuario.getCodUsuario());
+            if(u.getCodUsuario().equals(this.usuario.getCodUsuario())){
+                //System.out.println("Dentro del IF -- usurio entra a empresa "+u.getIdEmpresa().getNombre());
+                empresaList.add(u.getIdEmpresa());
+            }
+        }        
+        return empresaList;
+    };
+   
+    public void populateRoles(){
+        List<Usuarios> usuario      = usuario_EJB.findAll();        
+        List<Roles>    roles        = roles_EJB.findAll();           
+        List<UsuariosRoles> userRol = user_rolEJB.findAll();
+        
+        rolesList.clear();         
+        
+        //Seleccionando del listado el usuario introducico
+        for (Usuarios u : usuario){
+            //System.out.println(" dentro del for " + u.getCodUsuario()+ " usuario en variable " + this.usuario.getCodUsuario());
+            if(   u.getCodUsuario().equals(this.usuario.getCodUsuario())
+                && u.getIdEmpresa().getIdEmpresa().equals(this.emp.getIdEmpresa())
+                    ){
+                 System.out.println("Entramos a validar Usuario y empresa");
+                // Validando que rol tiene asignado ese usuario para la empresa seleccionada
+                for (UsuariosRoles userrol : userRol){
+                    System.out.println("En el for de roles "+userrol.getIdUserRole() + " "+u.getIdUsuario());
+                    if(   userrol.getIdUserRole().equals(u.getIdUsuario())                       
+                            ){
+                     //Asignando el rol del usuario para la empresa seleccionada
+                     System.out.println("Entramos a validar Rol y empresa... solo falta asignar "+userrol.getIdEmpresa().getIdEmpresa() + " rol " +userrol.getIdRol());
+                     for (Roles r: roles){
+                         System.out.println("Dentro del loop de roles " + r.getIdEmpresa().getIdEmpresa() + " rol = "+r.getIdRol() ) ;
+                         if(   r.getIdEmpresa().getIdEmpresa().equals(userrol.getIdEmpresa().getIdEmpresa())
+                            && r.getIdRol().equals(userrol.getIdRol())
+                                 ){
+                             System.out.println("Correr Desnudos por la casa Cesar Cansino!! ");
+                             rolesList.add(r);
+                         }
+                     }   
+                        
+
+
+                    }
+                    
+                }
+            }
+        }
+        
+        
+    }    
+    
     
 }
